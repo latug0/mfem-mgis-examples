d8c42a45f09d90d519912878a561496f4d4a795d
Guillaume Latu <guillaume.latu@ea.fr>
2021-04-11 15:40:41 +0200

    Configure the make install

--- config/ex1.cmake
+++ config/ex1.cmake
@@ -1,4 +1,6 @@
 set(MFEMMGIS_EX1 "${MFEMMGIS_DIR}/../examples/ex1")
+set(REF_DIR "${CMAKE_SOURCE_DIR}/ex1")
+
 file(COPY
   ${MFEMMGIS_EX1}/Plasticity.mfront
   ${MFEMMGIS_EX1}/cube.mesh
@@ -6,15 +8,12 @@ file(COPY
   ${MFEMMGIS_EX1}/UnitTestingUtilities.hxx
   DESTINATION ${CMAKE_BINARY_DIR}/ex1)
 
-#set(MFEMMGIS_TESTDIR "${MFEMMGIS_DIR}/../examples/ex1")
-#file(COPY ${MFEMMGIS_TESTDIR}/Plasticity.mfront
-#  DESTINATION ${CMAKE_CURRENT_BINARY_DIR}/)
+install(FILES
+  ${REF_DIR}/CMakeLists.txt
+  ${REF_DIR}/Makefile
+  ${MFEMMGIS_EX1}/Plasticity.mfront
+  ${MFEMMGIS_EX1}/cube.mesh
+  ${MFEMMGIS_EX1}/UniaxialTensileTest.cxx
+  ${MFEMMGIS_EX1}/UnitTestingUtilities.hxx
+  DESTINATION share/mfem-mgis-examples/ex1)
 
-#file(COPY ${MFEMMGIS_TESTDIR}/cube.mesh
-#  DESTINATION ${CMAKE_CURRENT_BINARY_DIR}/)
-
-#install(FILES
-#  ex1/CMakeLists.txt
-#  ex1/Makefile
-#  DESTINATION share/mfem-mgis/examples/ex1)
-#
--- config/ex2.cmake
+++ config/ex2.cmake
@@ -1,19 +1,15 @@
 set(MFEMMGIS_EX1 "${MFEMMGIS_DIR}/../examples/ex1")
+set(REF_DIR "${CMAKE_SOURCE_DIR}/ex2")
 file(COPY
   ${MFEMMGIS_EX1}/Plasticity.mfront
-  ${CMAKE_SOURCE_DIR}/ex2/ssna303.msh
+  ${REF_DIR}/ssna303.msh
   DESTINATION ${CMAKE_BINARY_DIR}/ex2)
 
-  #set(MFEMMGIS_TESTDIR "${MFEMMGIS_DIR}/../examples/ex2")
-#file(COPY
-#  ${MFEMMGIS_TESTDIR}/Plasticity.mfront
-#  ${MFEMMGIS_TESTDIR}/ssna303.msh
-#  DESTINATION ${CMAKE_CURRENT_BINARY_DIR}/)
-#
-#install(FILES
-#  ex2/CMakeLists.txt
-#  ex2/ssna303.cxx
-#  ${mfem-mgis-data_SOURCE_DIR}/mesh/ssna303.msh
-#  DESTINATION share/mfem-mgis/examples/ex2)
+install(FILES
+  ${REF_DIR}/CMakeLists.txt
+  ${MFEMMGIS_EX1}/Plasticity.mfront
+  ${REF_DIR}/ssna303.cxx
+  ${REF_DIR}/ssna303.msh
+  DESTINATION share/mfem-mgis-examples/ex2)
 
 
