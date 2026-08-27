e55d527fe3db085ed730fbc524b37aaaed69cef4
Thomas Helfer <thomas.helfer@cea.fr>
2026-08-27 09:54:16 +0200

    Fix Issue #4

--- ex4/ssna303_petsc.cxx
+++ ex4/ssna303_petsc.cxx
@@ -66,7 +66,7 @@ int main(int argc, char** argv) {
   {
     // loading the mesh and timer
     mfem_mgis::NonLinearEvolutionProblem problem(
-        {{"MeshFileName", mesh_file},
+      ctx,  {{"MeshFileName", mesh_file},
          {"FiniteElementFamily", "H1"},
          {"FiniteElementOrder", order},
          {"UnknownsSize", dim},
